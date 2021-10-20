fa7090dff4ea6814528ad0f1be102144ef5069ae
Nguyen Huy Hoang <llslla19712001@gmail.com>
2021-10-20 15:34:07 +0700

    hi

--- assets/controller/control.php
+++ assets/controller/control.php
@@ -1,7 +1,7 @@
 <?php
-	 $host = "localhost";
-     $user = "root";
-	 $password = "";
-	 $database = "testdb";
+	 $host = "mysql5031.site4now.net";
+     $user = "a7aa2a_hoangca";
+	 $password = "hoang2001";
+	 $database = "db_a7aa2a_hoangca";
      $conn = mysqli_connect($host,$user,$password,$database);
 ?>
--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-    $mysqli = new mysqli("localhost","root","","testdb");
+    $mysqli = new mysqli("mysql5031.site4now.net","a7aa2a_hoangca","hoang2001","db_a7aa2a_hoangca");
 
     // Check connection
     if ($mysqli->connect_errno) {
